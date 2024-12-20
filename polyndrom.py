43a3f6613b821414625e666616f6e0cab22a69cc
week101 <igor.sizov.07@mail.ru>
2024-12-20 20:45:12 +0300

    Update polyndrom.py

--- polyndrom.py
+++ polyndrom.py
@@ -1,12 +1,20 @@
 # Палиндром — это строка, которая читается одинаково слева направо и справа налево.
 # На вход программе подается строка.
 # Программа должна работать за O(n/2) без выделения лишней памяти.
-def is_palindrome(word):
-    return word.lower() == word[::-1].lower()
+def is_palindrom(word):
+    left = 0
+    right = len(word) - 1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
 
 input_word = input("Введите слово: ")
-
-if is_palindrome(input_word):
+if is_palindrom(input_word):
     print(f"Слово '{input_word}' является палиндромом.")
 else:
     print(f"Слово '{input_word}' не является палиндромом.")
